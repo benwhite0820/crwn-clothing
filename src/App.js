ea16654f7bff8a8703b7ce0de3ae9557d2c6d256
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2022-06-29 10:42:19 +0800

    create authentication page component alongside sign in form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import { Routes, Route } from 'react-router-dom';
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
+
 const Shop = () => {
     return (
         <div>
@@ -16,7 +17,7 @@ const App = () => {
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
                 <Route path="/shop" element={<Shop />} />
-                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-in" element={<Authentication />} />
             </Route>
         </Routes>
     );
---
+++ src/components/sign-in-form/sign-in-form-component.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    signInAuthUserWithEmailAndPassword,
+} from '../../utils/firebase/firebase.utils';
+import FormInput from '../form-input/form-input.component';
+import './sign-in-form.styles.scss';
+import Button from '../button/button.component';
+
+const defaultFormFields = {
+    email: '',
+    password: '',
+};
+
+const SignInForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields);
+
+    const { email, password } = formFields;
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    };
+
+    const signInWithGoogle = async () => {
+        const { user } = await signInWithGooglePopup();
+        // 儘管我們在db裡面還沒有任何collection, google還是會在path裡面給我們users/一長串uid  (我們在設定檔那邊有 console.log(userDocRef) )
+        await createUserDocumentFromAuth(user);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await signInAuthUserWithEmailAndPassword(
+                email,
+                password
+            );
+            console.log(response);
+            resetFormFields();
+        } catch (err) {
+            switch (err.code) {
+                case 'auth/wrong-password':
+                    alert('incorrect password for email');
+                    break;
+                case 'auth/user-not-found':
+                    alert('no user associated with this email');
+                    break;
+                default:
+                    console.log(err);
+            }
+        }
+        // error code =>auth/email-already-in-use error
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        console.log(formFields);
+
+        setFormFields({ ...formFields, [name]: value });
+    };
+    return (
+        <div className="sign-up-container">
+            <h2>Already have an account?</h2>
+            <span>Sign in with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    label="email"
+                    type="email"
+                    required
+                    onChange={handleChange}
+                    name="email"
+                    value={email}
+                />
+                <FormInput
+                    label="Password"
+                    type="password"
+                    required
+                    onChange={handleChange}
+                    name="password"
+                    value={password}
+                />
+                <div className="buttons-container">
+                    <Button type="submit">Sign In</Button>
+                    <Button
+                        type="button"
+                        buttonType="google"
+                        onClick={signInWithGoogle}
+                    >
+                        google sign in
+                    </Button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SignInForm;
